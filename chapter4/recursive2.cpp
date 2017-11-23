e8b85b2daa9d7a6c470f8c869db8687dfe38d1aa
Taotao Chen <collinschen0521@gmail.com>
2017-11-23 01:53:03 +0100

    modify recursive2.cpp

--- chapter4/recursive2.cpp
+++ chapter4/recursive2.cpp
@@ -8,11 +8,22 @@ int main()
     cout << f(n) << endl;
 }
 
+// int f(int n)
+// {
+//     int res = 0;
+//     if (n==1) res = 1;
+//     else if ( n == 2) res = 2;
+//     else res = f(n-1)*n;
+//
+//     return res;
+// }
 int f(int n)
 {
     int res = 0;
-    if (n==1) res = 1;
-    else if ( n == 2) res = 2;
+    if(n<0)
+    {cout << "error" <<endl;
+    res = -1;}
+    else if (n==0||n==1) res = 1;
     else res = f(n-1)*n;
 
     return res;

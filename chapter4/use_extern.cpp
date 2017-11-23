374147d638c494517e980d260a1456e408571201
Taotao Chen <collinschen0521@gmail.com>
2017-11-23 04:10:02 +0100

    add use_extern.cpp

---
+++ chapter4/use_extern.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int max(int, int);
+
+int main(int argc, char const *argv[]) {
+    extern int a,b;
+
+    cout << max(a,b) << endl;
+    return 0;
+}
+int a = 15, b = -7;
+int max(int x, int y)
+{
+    int z;
+    z=x>y? x:y;
+    return z;
+}

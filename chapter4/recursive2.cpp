c39e2aa6ca4d8acd057e4a288338c0105104a818
Taotao Chen <collinschen0521@gmail.com>
2017-11-23 01:46:02 +0100

    add recursive2.cpp

---
+++ chapter4/recursive2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int f(int n);
+int main()
+{   int n;
+    cin >> n;
+    cout << f(n) << endl;
+}
+
+int f(int n)
+{
+    int res = 0;
+    if (n==1) res = 1;
+    else if ( n == 2) res = 2;
+    else res = f(n-1)*n;
+
+    return res;
+}
